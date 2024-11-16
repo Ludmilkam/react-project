ae75b57b6c99ec6ec804ef2cbb5ee3a6adc6e76c
Liudmyla Makhynia <milam20090929@gmail.com>
2024-11-16 20:27:22 +0200

    created first react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function AppComponent(){
+    return (
+        <div>
+            <h1>Hello woda</h1>
+            <p>Hello wodiczka</p>
+            <img src="https://th.bing.com/th/id/OIP.JZOz6GGM0LNXxeaneIM0pwHaEo?rs=1&pid=ImgDetMain" alt="Photo wodiczki"></img>
+        </div>
+    )
 }
-
-export default App;
